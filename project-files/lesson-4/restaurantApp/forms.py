c1fcd6fe2bd14aab4dbe1035f68edb13d85645f6
Matthew <matthewdb85@gmail.com>
2016-02-20 23:36:21 +0000

    Adding tab navigation and add item button to newMenuItem.html

--- project-files/lesson-4/restaurantApp/forms.py
+++ project-files/lesson-4/restaurantApp/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import Form
 from wtforms import TextField, validators
 
 class restaurantForm(Form):
+	name = TextField("Name", [validators.InputRequired()])
 	address = TextField("Address", [validators.InputRequired()])
 	city = TextField("City", [validators.InputRequired()])
 	postCode = TextField("ZIP/Post code", [validators.InputRequired()])

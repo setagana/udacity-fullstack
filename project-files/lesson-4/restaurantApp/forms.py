1ad6afb40c317154b8f966a28821a52a024e9de0
Matthew <matthewdb85@gmail.com>
2016-02-22 09:23:04 +0000

    Adding first version of new menu item form

--- project-files/lesson-4/restaurantApp/forms.py
+++ project-files/lesson-4/restaurantApp/forms.py
@@ -8,3 +8,9 @@ class restaurantForm(Form):
 	postCode = TextField("ZIP/Post code", [validators.InputRequired()])
 	phone = TextField("Phone number", [validators.InputRequired()])
 	website = TextField("Website", [validators.URL()])
+
+class menuItemForm(Form):
+	dishName = TextField("Dish name", [validators.InputRequired()])
+	dishDescription = TextField("Description")
+	course = TextField("Course",[validators.InputRequired()])
+	price = TextField("Price", [validators.InputRequired()])
